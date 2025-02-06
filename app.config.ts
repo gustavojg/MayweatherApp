e438ce828202a17c82e07cab5ed2e8c618f3cf67
Gustavo <soygus@gmail.com>
2025-02-06 17:42:16 +0100

    change api keys

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    expo: {
-      extra: {
-        OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY,
-        WEATHERAPI_KEY: process.env.WEATHERAPI_KEY,
-      },
-    },
-  };
